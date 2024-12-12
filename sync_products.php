236b366207b18c8374b9e0dc1409497f1c0bfae2
vyma123 <mavanvy2023@gmail.com>
2024-12-12 14:20:14 +0700

    code ok using library

--- sync_products.php
+++ sync_products.php
@@ -3,7 +3,6 @@ require_once 'includes/db.inc.php';
 require 'includes/functions.php';
 require 'vendor/autoload.php';
 
-
 use Goutte\Client;
 use Symfony\Component\BrowserKit\Cookie;
 
@@ -15,6 +14,12 @@ $crawler = $client->request('GET', 'https://aliexpress.ru/item/1005007641037367.
     'HTTP_ACCEPT_LANGUAGE' => 'en-US,en;q=0.8', 
     'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 ]);
+
+
+
+
+
+
 
 $titles = [];  
 $colorArray = [];
